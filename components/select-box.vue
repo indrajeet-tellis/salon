dc775dd2d56a107825d907b2a3be8d323f9d62d1
urme-akter <112346871+urme-akter@users.noreply.github.com>
2023-08-27 18:16:13 +0600

    demo second page done

---
+++ components/select-box.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="select-box-main position-relative">
+    <div class="box-line d-flex justify-content-between">
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio1"
+          value="option1"
+        />
+        <label class="form-check-label" for="inlineRadio1"></label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio1"
+          value="option1"
+        />
+        <label class="form-check-label" for="inlineRadio1"></label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio1"
+          value="option1"
+        />
+        <label class="form-check-label" for="inlineRadio1"></label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio1"
+          value="option1"
+        />
+        <label class="form-check-label" for="inlineRadio1"></label>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  name: String,
+});
+</script>
+
+<style scoped>
+.form-check-inline {
+  display: inline-block;
+  margin-right: 0rem !important;
+  z-index: 2;
+}
+.form-check-input {
+  border-radius: 50%;
+  background: #2c3b91;
+}
+.form-check-input {
+  --bs-form-check-bg: var(--bs-body-bg);
+  width: 1em;
+  height: 1em;
+  margin-top: 0.25em;
+  vertical-align: top;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  /* background-color: var(--bs-form-check-bg); */
+  background-image: var(--bs-form-check-bg-image);
+  background-repeat: no-repeat;
+  background-position: center;
+  /* background-size: contain; */
+  border: 1px solid #2c3b91 !important;
+}
+.box-line::after {
+  content: "";
+  background: #2c3b91;
+  top: 37%;
+  left: 0%;
+  width: 98%;
+  height: 12%;
+  position: absolute;
+}
+</style>
--- pages/requestDemo/demo-book/index.vue
+++ pages/requestDemo/demo-book/index.vue
@@ -5,23 +5,66 @@
         <div class="col">
           <div class="py-5">
             <mobile-heading class="text-center" name="Book Demo" />
+            <div class="mt-3"><select-box /></div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <section>
-    <h3 class="demo-book-subHeading">Select your salon type</h3>
-    <demo-button class="mx-auto" name="Next" />
+  <section class="position-relative">
+    <div class="container" style="margin-bottom: 100px">
+      <h3 class="demo-book-subHeading">Select your salon type</h3>
+      <div class="row mt-4">
+        <div class="col-6 text-center">
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-one.png" /></div>
+            <h4 class="item-text">Hair</h4>
+          </div>
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-three.png" /></div>
+            <h4 class="item-text">Barbershop</h4>
+          </div>
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-five.png" /></div>
+            <h4 class="item-text">Spa</h4>
+          </div>
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-seven.png" /></div>
+            <h4 class="item-text">Waxing</h4>
+          </div>
+        </div>
+        <div class="col-6 text-center">
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-two.png" /></div>
+            <h4 class="item-text">Beauty</h4>
+          </div>
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-four.png" /></div>
+            <h4 class="item-text">Nails</h4>
+          </div>
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-six.png" /></div>
+            <h4 class="item-text">Eyebrow</h4>
+          </div>
+          <div class="item">
+            <div class="type-img"><img src="/assets/type-eight.png" /></div>
+            <h4 class="item-text">Massage</h4>
+          </div>
+        </div>
+      </div>
+      <demo-button class="mx-auto btn" name="Next" />
+    </div>
   </section>
 </template>
 <script>
 import MobileHeading from "../../../components/mobile-heading.vue";
 import DemoButton from "../../../components/demo-button.vue";
+import SelectBox from "../../../components/select-box.vue";
 export default {
   components: {
     MobileHeading,
     DemoButton,
+    SelectBox,
   },
   setup() {
     let database = ref([
@@ -45,5 +88,43 @@ export default {
   font-family: "Roboto", sans-serif;
   text-align: center;
   margin: 10px auto;
+}
+.label-box {
+  margin-top: 20px;
+}
+.item {
+  width: 45%;
+  background: #fff;
+  margin: 0 auto;
+  padding: 8px 10px;
+  -webkit-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.23);
+  -moz-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.23);
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.23);
+  margin-top: 20px;
+  border-radius: 5px;
+  transition: 0.4s;
+}
+.item:hover {
+  background: #2c3b91;
+}
+.item:hover h4 {
+  color: #fff;
+}
+
+.type-img img {
+  width: 100%;
+}
+.item-text {
+  font-size: 15px;
+  font-weight: 500;
+  color: #2c3b91;
+  margin-top: 9px;
+  font-family: "DM Sans", sans-serif;
+  margin-bottom: 0 !important;
+}
+.btn {
+  position: fixed;
+  bottom: 20%;
+  left: 20%;
 }
 </style>
