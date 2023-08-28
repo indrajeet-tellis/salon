5ff04dee28f254d9ba9eea677f373d0b6a3eed05
urme-akter <112346871+urme-akter@users.noreply.github.com>
2023-08-28 13:43:24 +0600

    demo 6th page done

---
+++ components/lottie-animation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ani-main">
+    <div class="ani-box">
+      <client-only>
+        <Vue3Lottie
+          animationLink="https://lottie.host/0f621e40-0aeb-4eaa-a0e3-72d1d3a7b096/cV7NoRllOz.json"
+          :height="200"
+          :width="200"
+        />
+      </client-only>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.ani-box {
+  margin: 0 auto;
+}
+</style>
---
+++ pages/requestDemo/demo-successfully/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <section
+    class="demoBG"
+    :style="{ backgroundImage: 'url(/assets/demo-bg.png)' }"
+  >
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <lottie-animation />
+          <div class="text-center" style="margin-top: 19px">
+            <h1 class="mb-0 demo-text">Thank You !!!</h1>
+            <p class="mb-0 demo-subText">
+              You have successfully book your demo request
+            </p>
+          </div>
+          <demo-button name="OK" class="ok-btn" style="margin: 0 auto" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import DemoButton from "../../../components/demo-button.vue";
+import LottieAnimation from "../../../components/lottie-animation.vue";
+
+export default {
+  components: {
+    DemoButton,
+    LottieAnimation,
+  },
+
+  setup() {
+    let database = ref([
+      {
+        name: "string",
+      },
+    ]);
+  },
+};
+</script>
+<style scoped>
+.demoBG {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-bottom: 100px;
+  padding: 190px 0;
+  position: relative;
+  height: auto;
+}
+.ok-btn {
+  position: fixed;
+  bottom: 23%;
+  left: 22%;
+}
+.demo-text {
+  font-size: 40px;
+  font-weight: 700;
+  font-family: "Roboto", sans-serif;
+  color: #2c3b91;
+}
+.demo-subText {
+  font-size: 18px;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  color: #2c3b91;
+  width: 221px;
+  margin: 20px auto 0 auto;
+  line-height: 20px;
+}
+</style>
